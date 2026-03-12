f8154ab2123d599aef3ae65cb6a0c13ab72245fe
nwlterry <nwl.terry@gmail.com>
2026-03-12 14:08:09 +0800

    Add UNC Connectivity Checker scheduled task
    Registers a scheduled task to run a PowerShell script for UNC connectivity checking.

---
+++ IIS_Cert_Auto_Package/unc_checker_schedule_task.ps1
@@ -0,0 +1,7 @@
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\Scripts\UNC-Connectivity-Checker.ps1"'
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$tempTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Hours 1)
+$trigger.Repetition = $tempTrigger.Repetition
+$principal = New-ScheduledTaskPrincipal -GroupId "NT AUTHORITY\SYSTEM" -RunLevel Highest
+$settings = New-ScheduledTaskSettingsSet
+Register-ScheduledTask -TaskName "UNC Connectivity Checker" -Action $action -Trigger $trigger -Principal $principal -Settings $settings -Description "Runs a PowerShell script hourly after system startup."
