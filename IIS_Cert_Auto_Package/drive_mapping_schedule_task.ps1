1fe15a9ddb6cba34c6568e156e2949e45c789b6b
nwlterry <nwl.terry@gmail.com>
2026-04-30 16:10:47 +0800

    Update drive_mapping_schedule_task.ps1

--- IIS_Cert_Auto_Package/drive_mapping_schedule_task.ps1
+++ IIS_Cert_Auto_Package/drive_mapping_schedule_task.ps1
@@ -1,3 +1,5 @@
-$action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument '/c C:\Scripts\drive_mapping.bat'
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\Scripts\drive_mapping.ps1"'
 $trigger = New-ScheduledTaskTrigger -AtStartup
-Register-ScheduledTask -TaskName "ACME Map Drive As System" -Action $action -Trigger $trigger -User "SYSTEM" -RunLevel Highest -Force
+$principal = New-ScheduledTaskPrincipal -GroupId "NT AUTHORITY\SYSTEM" -RunLevel Highest
+$settings = New-ScheduledTaskSettingsSet
+Register-ScheduledTask -TaskName "ACME Map Drive As System" -Action $action -Trigger $trigger -Principal $principal -Settings $settings -Description "Runs a PowerShell script after system startup."
